e0ce96cad749a41d32c414d91307f70d228a40d4
larsrinn <lm.rinn@outlook.com>
2018-03-03 15:59:11 +0100

    removed "usual" app

--- src/main.js
+++ src/main.js
@@ -3,17 +3,9 @@
 import Vue from 'vue'
 import wrap from '@vue/web-component-wrapper'
 import MarkdownEditor from './components/MarkdownEditor'
-import App from './App'
 
 Vue.config.productionTip = false
 
 const MarkdownEditorElement = wrap(Vue, MarkdownEditor)
 
 window.customElements.define('markdown-editor', MarkdownEditorElement)
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
