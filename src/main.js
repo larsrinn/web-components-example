01d6a7800ae57be3a99ba16e85393fd6de7fc0ba
larsrinn <lm.rinn@outlook.com>
2018-03-03 13:45:19 +0100

    markdown editor as single file component - css not working yet

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import wrap from '@vue/web-component-wrapper'
+import MarkdownEditor from './components/MarkdownEditor'
 
 Vue.config.productionTip = false
 
@@ -15,5 +16,7 @@ const ClickCountingButton = {
 }
 
 const ClickCountingButtonElement = wrap(Vue, ClickCountingButton)
+const MarkdownEditorElement = wrap(Vue, MarkdownEditor)
 
 window.customElements.define('click-counting-button', ClickCountingButtonElement)
+window.customElements.define('markdown-editor', MarkdownEditorElement)
