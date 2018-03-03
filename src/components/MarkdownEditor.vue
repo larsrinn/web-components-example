212eb24b9a97c75bff178c8850488e29f682570e
larsrinn <lm.rinn@outlook.com>
2018-03-03 15:51:35 +0100

    some css

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="editor">
-    <h3>Markdown Editor</h3>
+    <h3 :class="$style.red">Markdown Editor</h3>
     <textarea v-model="input"></textarea>
     <vue-markdown :source="input"></vue-markdown>
   </div>
@@ -21,39 +21,8 @@ export default {
 }
 </script>
 
-<style scoped>
-#editor {
-  margin: 0;
-  height: 100%;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
-  color: #333;
-}
-
-h3 {
+<style module>
+.red {
   color: red;
-}
-
-textarea, #editor div {
-  display: inline-block;
-  width: 49%;
-  height: 100%;
-  vertical-align: top;
-  box-sizing: border-box;
-  padding: 0 20px;
-}
-
-textarea {
-  border: none;
-  border-right: 1px solid #ccc;
-  resize: none;
-  outline: none;
-  background-color: #f6f6f6;
-  font-size: 14px;
-  font-family: 'Monaco', courier, monospace;
-  padding: 20px;
-}
-
-code {
-  color: #f66;
 }
 </style>
