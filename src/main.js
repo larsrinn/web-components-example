1534e965522dc895617f0dd76a046ae4e032528d
larsrinn <lm.rinn@outlook.com>
2018-03-03 12:57:40 +0100

    not using vue instance anymore

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import wrap from '@vue/web-component-wrapper'
 
 Vue.config.productionTip = false
 
-const Component = {
+const ClickCountingButton = {
   template: '<span>Click counting button <button v-on:click="counter += 1">{{ counter }}</button></span>',
-  // data is technically a function, so Vue won't
-  // complain, but we return the same object
-  // reference for each component instance
   data: function () {
     return {
       counter: 0
@@ -18,13 +14,6 @@ const Component = {
   }
 }
 
-const CustomElement = wrap(Vue, Component)
+const ClickCountingButtonElement = wrap(Vue, ClickCountingButton)
 
-window.customElements.define('my-element', CustomElement)
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+window.customElements.define('click-counting-button', ClickCountingButtonElement)
